5095ff1eb3dcba5827d62bb8d4e9cbe84eda28f6
Keenan Diggs <diggs.dev@gmail.com>
2025-04-29 05:09:07 -0500

    add/update new portfolio app

--- CharacterSelectApp/main.js
+++ CharacterSelectApp/main.js
@@ -1,4 +1,29 @@
 const vowels = document.getElementsByClassName("letter");
+const music = new Audio("./assets/fight_song.mp3");
+music.loop = true;
+const selectEffect = new Audio("./assets/select_fx.mp3");
+
+const mute = document.getElementById("mute");
+const muteIcon = document.getElementById("mute-icon");
+
+let isUserActivated = false;
+let isMusicPlaying = false;
+
+mute.addEventListener("click", () => {
+    music.volume = (isMusicPlaying) ? 0 : 1;
+    muteIcon.src = (isMusicPlaying) ? "./assets/icons/volume-mute.png" : "./assets/icons/volume.png" ;
+    isMusicPlaying = !isMusicPlaying;
+});
+
+document.addEventListener(("click"), () => {
+     if (!isUserActivated) {
+        isUserActivated = true;
+        music.play();
+        isMusicPlaying = true;
+
+        muteIcon.src = "./assets/icons/volume.png";
+    }
+});
 
 const onVowelClick = (event) =>  {
     for (let i = 0; i < vowels.length; i++) {
@@ -9,6 +34,10 @@ const onVowelClick = (event) =>  {
             const platform = document.getElementById("platform");
             platform.innerText = vowels[i].innerText;
         }
+    }
+
+    if (isMusicPlaying) {
+        (new Audio("./assets/select_fx.mp3")).play();
     }
 };
 
