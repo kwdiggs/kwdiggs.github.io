8c018e481b3d784288295452e99b977052ec88da
Keenan Diggs <diggs.dev@gmail.com>
2025-04-30 04:22:13 -0500

    add portfolio page; refactor DVD Scren Saver and add youre welcome mssg

--- DVDScreenSaver/script.js
+++ DVDScreenSaver/script.js
@@ -1,2 +1,10 @@
-// animate ball
+const message = document.getElementById("message");
+let messageVisible = false;
+message.style.visibility = Boolean(messageVisible) ? "visible" : "hidden";
 
+function toggleMessage() {
+    message.style.visibility = Boolean(messageVisible) ? "visible" : "hidden";
+    messageVisible = !(messageVisible);
+}
+
+setTimeout(toggleMessage, 8500);
