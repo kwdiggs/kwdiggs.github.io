d9c629a21d31f0d865eb32dbd4233a91c9838919
Keenan Diggs <diggs.dev@gmail.com>
2025-04-30 04:27:40 -0500

    change youre welcome message to appear only once and then leave

--- DVDScreenSaver/script.js
+++ DVDScreenSaver/script.js
@@ -1,10 +1,11 @@
 const message = document.getElementById("message");
 let messageVisible = false;
-message.style.visibility = Boolean(messageVisible) ? "visible" : "hidden";
 
 function toggleMessage() {
     message.style.visibility = Boolean(messageVisible) ? "visible" : "hidden";
     messageVisible = !(messageVisible);
 }
 
+toggleMessage();
 setTimeout(toggleMessage, 8500);
+setTimeout(toggleMessage, 14000);
