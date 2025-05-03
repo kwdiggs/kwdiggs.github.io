c86a3bb2f14b59ed855bfac7acceb64df351f1f4
Keenan Diggs <diggs.dev@gmail.com>
2025-05-02 19:36:12 -0500

    test game idea

---
+++ SingMan/scripts/dialog.js
@@ -0,0 +1,27 @@
+const dialog = document.querySelector("dialog");
+const closeButton = document.getElementById("dialog-close-button");
+const newgameButton = document.getElementById("new-game");
+const finalMessage = document.getElementById("final-message");
+
+const closeDialogFn = (() => dialog.close());
+const refreshPageFn = (() => window.location.reload());
+
+closeButton.addEventListener("click", closeDialogFn);
+newgameButton.addEventListener("click", refreshPageFn);
+
+function showEndingModal(solved) {
+    const dialog = document.getElementById("ending-modal");
+    const message = document.getElementById("final-message");
+    const backgroundUrl = Boolean(solved)
+        ? "../assets/victory.png"
+        : "../assets/defeat.png";
+
+    dialog.style.backgroundImage= "url(" + backgroundUrl  + ")";
+    message.innerText = Boolean(solved)
+        ? "You Win!"
+        : "Game Over! Try Again.";
+
+    dialog.showModal();
+}
+
+export { dialog, showEndingModal };
